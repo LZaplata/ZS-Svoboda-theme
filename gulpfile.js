b5198fccb4ecc1cdc138fea04405e37f225a3689
zap <zap@hucr.cz>
2022-01-25 12:35:16 +0100

    theme initialization

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+let {src, dest, watch, parallel} = require("gulp");
+let sass = require("gulp-sass");
+let minify = require("gulp-clean-css");
+let themeDir = "./themes/lzaplata-zssvoboda";
+
+function sassTask() {
+    return src(themeDir + "/assets/sass/theme.sass")
+        .pipe(sass({outputStyle: "compressed", includePaths: ["./node_modules"]}))
+        .pipe(dest(themeDir + "/assets/css"));
+}
+
+function cssTask() {
+    return src([
+        "node_modules/bootstrap-icons/font/bootstrap-icons.css",
+        "node_modules/swiper/swiper-bundle.css",
+    ])
+        .pipe(minify())
+        .pipe(dest(themeDir + "/assets/css"))
+}
+
+function fontsTask() {
+    return src("node_modules/bootstrap-icons/font/fonts/*")
+        .pipe(dest(themeDir + "/assets/css/fonts"))
+}
+
+function jsTask() {
+    return src([
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/bootstrap/dist/js/bootstrap.js",
+        "node_modules/@popperjs/core/dist/umd/popper.js",
+        "node_modules/swiper/swiper-bundle.js",
+    ])
+        .pipe(dest(themeDir + "/assets/js"))
+}
+
+function watchTask() {
+    watch([
+        themeDir + "/assets/**/theme.sass",
+        themeDir + "/assets/**/_variables.sass",
+    ], {ignoreInitial: false}, sassTask);
+}
+
+exports.default = parallel(sassTask, cssTask, fontsTask, jsTask);
+exports.watch = watchTask;
